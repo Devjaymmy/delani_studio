242b687c10ac1af3cf42379a2801fd558f048720
James Ng'ang'a <james.ng'ang'a@student.moringaschool.com>
2021-12-18 13:41:43 +0300

    created toggle between what we do icons and descriptions

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".design").click(function (event) {
+    $("#description-design").toggle();
+  });
+  $(".dev").click(function (event) {
+    $("#description-dev").toggle();
+  });
+  $(".product").click(function (event) {
+    $("#description-product").toggle();
+  });
+});
