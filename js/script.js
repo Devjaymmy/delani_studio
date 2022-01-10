275413f669ef77ddf63c37085079398c23dcd09d
James Ng'ang'a <james.ng'ang'a@student.moringaschool.com>
2022-01-10 12:10:16 +0300

    added hover effect on portfolio images

--- js/script.js
+++ js/script.js
@@ -11,7 +11,49 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-  $(".porti").hover(function (over) {
-    $(".whiteBox").toggle();
+  $("#port1").hover(function () {
+    $("#overlay1").toggle();
+  });
+});
+
+$(document).ready(function () {
+  $("#port2").hover(function () {
+    $("#overlay2").toggle();
+  });
+});
+
+$(document).ready(function () {
+  $("#port3").hover(function () {
+    $("#overlay3").toggle();
+  });
+});
+
+$(document).ready(function () {
+  $("#port4").hover(function () {
+    $("#overlay4").toggle();
+  });
+});
+
+$(document).ready(function () {
+  $("#port5").hover(function () {
+    $("#overlay5").toggle();
+  });
+});
+
+$(document).ready(function () {
+  $("#port6").hover(function () {
+    $("#overlay6").toggle();
+  });
+});
+
+$(document).ready(function () {
+  $("#port7").hover(function () {
+    $("#overlay7").toggle();
+  });
+});
+
+$(document).ready(function () {
+  $("#port8").hover(function () {
+    $("#overlay8").toggle();
   });
 });
